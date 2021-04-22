490b86ded33d0217575feec5bd31fc1e62de6b0c
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-22 15:17:22 +0600

    h-w complete

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -5,11 +5,14 @@ const Product = (props) => {
   const { name, img, price } = product;
 
   return (
-    <div className="col-md-3">
-      <div className="m-2">
+    <div
+      className="col-md-3 mb-3
+    "
+    >
+      <div className="m-3 shadow rounded p-3 h-100">
         <img className="img-fluid" src={img} alt="" />
         <h3> {name}</h3>
-        <h5>{price} </h5>
+        <h5>Price:{price} Taka</h5>
       </div>
     </div>
   );
