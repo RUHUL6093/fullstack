d71e365b26b25d14d1c580908123b9fe2ee342f1
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-22 15:08:36 +0600

    ui appear

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,6 +1,17 @@
 import React from "react";
 
-const Product = () => {
-  return <div>i am a single Product</div>;
+const Product = (props) => {
+  const product = props.product;
+  const { name, img, price } = product;
+
+  return (
+    <div className="col-md-3">
+      <div className="m-2">
+        <img className="img-fluid" src={img} alt="" />
+        <h3> {name}</h3>
+        <h5>{price} </h5>
+      </div>
+    </div>
+  );
 };
 export default Product;
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,10 +1,11 @@
 import React from "react";
 import productData from "../../Components/fakeData/fakedata.json";
+import Product from "../Product/Product";
 const Products = () => {
   return (
     <div className="row">
       {productData.map((p) => (
-        <h4> {p.name}</h4>
+        <Product product={p}></Product>
       ))}
     </div>
   );
