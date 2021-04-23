3c4c98acc0f119c82a637ab0410eab284448db58
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-04-23 15:29:04 +0600

    update url

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,8 +2,16 @@ import React from "react";
 import productData from "../../Components/fakeData/fakedata.json";
 import Product from "../Product/Product";
 const Products = () => {
+  const addAllProducts = () => {
+    fetch("https://jsxuo.sse.codesandbox.io/addProducts", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: "React POST Request Example" })
+    });
+  };
   return (
     <div className="row">
+      <button onClick={addAllProducts}>Add All</button>
       {productData.map((p) => (
         <Product product={p}></Product>
       ))}
