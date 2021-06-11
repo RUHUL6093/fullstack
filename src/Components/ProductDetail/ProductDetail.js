21a4f68451b4f31683c9dd3c2751755415c9132e
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-06-11 22:00:53 +0600

    ok

--- src/Components/ProductDetail/ProductDetail.js
+++ src/Components/ProductDetail/ProductDetail.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 const ProductDetail = () => {
   const { Name } = useParams();
+  const [detail, setDetail] = useState({});
+  useEffect(() => {
+    // const url = `https://t5ext.csb.app/product/{Name}`;
+    fetch(`https://t5ext.csb.app/product/${Name}`)
+      .then((res) => res.json())
+      .then((data) => setDetail(data));
+  });
+
   return (
     <div>
-      <h1>This is Detail of product :{Name}</h1>
+      <h1>This is Detail :{Name}</h1>
+      <h3> {detail.name}</h3>
+      {/* <h4> Price:{detail.price}</h4> */}
     </div>
   );
 };
